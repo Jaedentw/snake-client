afc984a4c4f45c490074001c1230936497918371
Jaeden West <jaeden.t.west@gmail.com>
2022-04-21 16:09:27 -0600

    input.js added and required by play.js

--- play.js
+++ play.js
@@ -1,18 +1,4 @@
-const connect = require("./client");
-
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-}
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 setupInput();
