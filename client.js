0ece446da1a7177c7da461bfed91c91c67af5048
Jaeden West <jaeden.t.west@gmail.com>
2022-04-21 16:52:28 -0600

    added constants.js for static data

--- client.js
+++ client.js
@@ -1,10 +1,11 @@
 const net = require("net");
+const { IP, PORT } = require("./constants")
 
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: '192.168.242.211' ,
-    port: '50541',
+    host: IP ,
+    port: PORT,
   });
 
   // interpret incoming data as text
---
+++ constants.js
@@ -0,0 +1,8 @@
+const PORT = '50541';
+const IP = '192.168.242.211';
+
+module.exports = {
+  IP,
+  PORT,
+  
+}
