94738aa21bdf08392e04b223ba02a1fd01441257
Jaeden West <jaeden.t.west@gmail.com>
2022-04-24 13:31:30 -0600

    Added readme file with basic description

--- client.js
+++ client.js
@@ -1,8 +1,8 @@
 const net = require("net");
-const { IP, PORT } = require("./constants")
+const { IP, PORT } = require("./constants");
 
 // establishes a connection with the game server
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
     host: IP ,
     port: PORT,
@@ -12,13 +12,13 @@ const connect = function () {
   conn.setEncoding("utf8");
 
   conn.on("connect", () => {
-    console.log("connected");
-    conn.write('Name: JTW')
+    console.log("connected...");
+    conn.write('Name: JTW');
   });
 
   conn.on("data", (data) => {
     console.log(data);
-  })
+  });
   
   return conn;
 };
--- input.js
+++ input.js
@@ -1,12 +1,12 @@
 let connection;
 
-const setupInput = function (conn) {
+const setupInput = function(conn) {
   connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-  const handleUserInput = function (key) {
+  const handleUserInput = function(key) {
     if (key === '\u0003') {
       process.exit();
     }
@@ -34,7 +34,7 @@ const setupInput = function (conn) {
     if (key === '0') {
       conn.write('Say: :)');
     }
-  }
+  };
   stdin.on("data", handleUserInput);
   return stdin;
 };
